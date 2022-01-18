7c45553774dae7be057dc2df502e783b6e21fa10
gus <ggortiz09@gmail.com>
2022-01-17 22:59:05 -0300

    first commit

---
+++ src/test/java/com/gortiz/batch/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gortiz.batch.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

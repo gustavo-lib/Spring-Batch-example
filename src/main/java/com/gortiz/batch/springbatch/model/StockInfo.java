4cd1f862fb8b7cce570835a8102ec2bdecd19f75
gus <ggortiz09@gmail.com>
2022-01-17 23:10:42 -0300

    config-lister-model-step

---
+++ src/main/java/com/gortiz/batch/springbatch/model/StockInfo.java
@@ -0,0 +1,14 @@
+package com.gortiz.batch.springbatch.model;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class StockInfo {
+    private String stockId;
+    private String stockName;
+    private double stockPrice;
+    private double yearlyHigh;
+    private double yearlyLow;
+    private String address;
+    private String sector;
+    private String market;

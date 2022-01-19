615e07cd27d6dcf01cb5c614aa4d72667f4aa835
gus <ggortiz09@gmail.com>
2022-01-19 00:04:36 -0300

    step - bug lombok

--- src/main/java/com/gortiz/batch/springbatch/step/StockInfoProcessor.java
+++ src/main/java/com/gortiz/batch/springbatch/step/StockInfoProcessor.java
@@ -1,9 +1,20 @@
 package com.gortiz.batch.springbatch.step;
 
+import com.gortiz.batch.springbatch.model.StockInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
 
-public class StockInfoProcessor {
+public class StockInfoProcessor  implements ItemProcessor<StockInfo, String> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(StockInfoProcessor.class);
+
+    @Override
+    public String process(StockInfo stockInfo) throws Exception {
+        System.out.println("Hello");
+        String message = stockInfo.getStockName() + " is trading at  "
+                + stockInfo.getStockPrice() + " on " + stockInfo.getMarket()+" at "+ new Date().toString()+ "!";
+        LOGGER.info("printing '{}' to output file", message);
+        return message;
+    }
 }

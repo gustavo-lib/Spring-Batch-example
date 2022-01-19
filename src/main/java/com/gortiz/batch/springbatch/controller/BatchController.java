90d3fc19a73ebf2f12852688d25581c49c747182
gus <ggortiz09@gmail.com>
2022-01-18 23:31:47 -0300

    new end point

--- src/main/java/com/gortiz/batch/springbatch/controller/BatchController.java
+++ src/main/java/com/gortiz/batch/springbatch/controller/BatchController.java
@@ -1,7 +1,29 @@
 package com.gortiz.batch.springbatch.controller;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BatchController {
+
+    @Autowired
+    JobLauncher jobLauncher;
+
+    @Autowired
+    Job stockpricesinfojob;
+
+
+    @GetMapping("/job")
+    public String invokeJob() throws Exception {
+        JobParameters jobParameters = new JobParametersBuilder().addLong("time", System.currentTimeMillis())
+                .toJobParameters();
+        jobLauncher.run(stockpricesinfojob, jobParameters);
+        return "Batch job has been invoked ;) ...";
+    }
+
 }

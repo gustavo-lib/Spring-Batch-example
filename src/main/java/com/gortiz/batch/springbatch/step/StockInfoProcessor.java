c7b29cf0ff72d506aeb3489c500ae30500d59d02
gus <ggortiz09@gmail.com>
2022-01-18 23:47:58 -0300

    step

---
+++ src/main/java/com/gortiz/batch/springbatch/step/StockInfoProcessor.java
@@ -0,0 +1,9 @@
+package com.gortiz.batch.springbatch.step;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StockInfoProcessor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockInfoProcessor.class);
+}
